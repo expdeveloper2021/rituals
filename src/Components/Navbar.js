99430c01b5f34ccbc4f332fe1150a41f758b8836
= <=>
2020-04-05 10:43:34 +0500

    First Commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react'
+import logo from '../assets/logowhite.png'
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import CloseIcon from '@material-ui/icons/Close';
+import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
+import ShoppingCartRoundedIcon from '@material-ui/icons/ShoppingCartRounded';
+import './index.css'
+
+export class Navbar extends Component {
+    constructor() {
+        super()
+        this.state = {
+            active: false,
+            active1: true,
+            active2: false,
+            active3: false,
+            active4: false,
+            active5: false,
+            active6: false,
+            search: false,
+            closeDisplay: false,
+        }
+    }
+
+    render() {
+        return (
+            <div className="header-main">
+                <div className="header-top">
+                    <ul>
+                        <li>
+                            <a>
+                                KLANTENSERVICE
+                            </a>
+                        </li>
+                        <li className="blt-list">
+                            <a>
+                                WINKELZOEKER
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="header-bottom">
+                    <div className="lists-hb">
+                        <button className="navbar-toggler d-sm-block d-md-none" type="button" onClick={() => this.setState({ active: !this.state.active })} style={{ outline: "none" }}>
+                            {this.state.active ? <CloseIcon style={{ fontSize: 22, fill: "black" }} /> : <MenuIcon className="menuopen" style={{ fontSize: 22, fill: "white" }} />}
+                            <a href="#" className="mobilemenu-close"></a>
+                        </button>
+                        <header className={`header1 ${!!this.state.active ? 'headeractive' : ''}`}>
+                            <nav className="navbar navbar-expand-lg">
+                                <div className="mymenu" id="navbarSupportedContent">
+                                    <ul className="navbar-nav slideractive" id="menu">
+                                        <li className="nav-item">
+                                            <a className="nav-link">Producten</a>
+                                        </li>
+                                        <li className="nav-item">
+                                            <a className="nav-link">Collecties</a>
+                                        </li>
+                                        <li className="nav-item">
+                                            <a className="nav-link">Cadeaus</a>
+                                        </li>
+                                        <li className="nav-item">
+                                            <a className="nav-link">Magazine</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </nav>
+                        </header>
+                    </div>
+                    <div className="heading-hb">
+                        <img src={logo} height="20" width="80" />
+                    </div>
+                    <div className="right-hb">
+                        <div className="inut-rhbn">
+                            <SearchIcon style={{ color: "white", fontSize: 30 }} />
+                            <input placeholder="Ik zoek..." className="search" onFocus={() => this.setState({ closeDisplay: !this.state.closeDisplay })} onBlur={() => this.setState({ closeDisplay: !this.state.closeDisplay })} />
+                            {this.state.closeDisplay && <CloseIcon style={{ color: "white" }} />}
+                        </div>
+                        <span>
+                            <AccountCircleOutlinedIcon style={{ fill: "white", fontSize: 28 }} />
+                        </span>
+                        <span>
+                            <ShoppingCartRoundedIcon style={{ fill: "white", fontSize: 28 }} />
+                        </span>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Navbar
---
+++ src/Containers/Home/index.js
@@ -0,0 +1,222 @@
+import React, { Component } from 'react'
+import SlowMotionVideoIcon from '@material-ui/icons/SlowMotionVideo';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+import './index.css'
+
+AOS.init();
+
+export class Home extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            transition: false,
+            arr: ["1", "2", "3", "4", "5", "6"]
+        }
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({ transition: true })
+        }, 200);
+        setTimeout(() => {
+            this.setState({ transition: false })
+        }, 300);
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="bg-main-firsth">
+                    <div
+                        className="full-bg-hf"
+                        style={{
+                            backgroundImage: "url('https://www.rituals.com/on/demandware.static/-/Library-Sites-RitualsSharedContent/default/dw982060e6/COP-banners/2020/easter-2020/Homepage-hero-banner-regular_S_1440x480.jpg')",
+                            transform: this.state.transition ? "scale(1.1)" : "scale(1)"
+                        }}>
+                        <div className="inner-wrapper" style={{ zIndex: 100 }}>
+                            <div className="inner-intended">
+                                <div className="ico-logorh d-none d-md-block">
+                                </div>
+                                <h1 className="heading-bhf txt-white" >Een paasverrassing</h1>
+                                <p className="txt-white">Deel een luxueus cadeau, speels verpakt als een paasei</p>
+                                <button>VUL JE MAND</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="start-sec-second">
+                    <p className="heading-hss">AT YOUR SERVICE</p>
+                    <p className="cont-hss">Levering 2-3 dagen in verband met drukte</p>
+                </div>
+                <div className="card-sec-rit">
+                    <div className="inner-wrapper">
+                        <div className="all-cards-rit">
+                            <div className="card-rit" data-aos="fade-in">
+                                <div className="content-crit">
+                                    <span style={{ textTransform: "uppercase" }} className="heading-hss">EXCLUSIEF VOOR MY RITUALS MEMBERS</span>
+                                    <h3>Ons cadeau voor jou</h3>
+                                    <p className="p-cr">Ontvang een Jing set, <br /> bij besteding vanaf €40.</p>
+                                    <button>SHOP BESTSELLERS</button>
+                                </div>
+                                <div className="img-crit">
+                                    <img src="./assets/second.jpg" width="100%" height="100%" />
+                                </div>
+                            </div>
+                            <div className="card-rit" data-aos="fade-in">
+                                <div className="content-crit">
+                                    <h3>The Ritual of Shakura</h3>
+                                    <p className="p-cr">Voor iedereen is de perfecte geur te vinden in deze uitgebreide collectie van rijke, frisse en bloemige geuren voor in huis.</p>
+                                    <button>SHOP NU</button>
+                                </div>
+                                <div className="img-crit">
+                                    <img src="./assets/first.jpg" width="100%" height="100%" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="third-sec-main">
+                    <div className="slider-products">
+                        <div className="sp-card hiddenMob">
+                            <h2>Favorieten in spe</h2>
+                            <h3>Nieuw binnen</h3>
+                            <p>Begin je dag met een heerlijk <br /> ontspannende verzorgingsroutine en <br /> verwen je huid met jouw favoriete producten.</p>
+                            <button>SHOP NU</button>
+                        </div>
+                        {this.state.arr.map((e) => {
+                            return <div className="sp-card" key={e}>
+                                <div className="img-cardsp">
+                                    <img src={`./assets/product${e}.webp`} width="100%" height="100%" />
+                                </div>
+                                <div className="content-cardsp">
+                                    <h1 className="product-title">Easter Egg Giftset</h1>
+                                    <p className="product-price">€8,50</p>
+                                </div>
+                            </div>
+                        })}
+                    </div>
+                </div>
+                <div className="card-sec-rit">
+                    <div className="inner-wrapper">
+                        <div className="all-cards-rit">
+                            <div className="card-rit" data-aos="fade-in">
+                                <div className="content-crit">
+                                    <h3>Ons cadeau voor jou</h3>
+                                    <p className="p-cr">Voor iedereen is de perfecte geur te vinden in deze uitgebreide collectie van rijke, frisse en bloemige geuren voor in huis.</p>
+                                    <button>SHOP BESTSELLERS</button>
+                                </div>
+                                <div className="img-crit">
+                                    <img src="./assets/fifth.jpg" width="100%" height="100%" />
+                                </div>
+                            </div>
+                            <div className="card-rit" data-aos="fade-in">
+                                <div className="content-crit">
+                                    <h3>The Ritual of Shakura</h3>
+                                    <p className="p-cr">Voor iedereen is de perfecte geur te vinden in deze uitgebreide collectie van rijke, frisse en bloemige geuren voor in huis.</p>
+                                    <button>SHOP NU</button>
+                                </div>
+                                <div className="img-crit">
+                                    <img src="./assets/sixth.jpg" width="100%" height="100%" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="third-sec-main">
+                    <div className="slider-products">
+                        <div className="sp-card hiddenMob">
+                            <h2>Favorieten in spe</h2>
+                            <h3>Nieuw binnen</h3>
+                            <p>Begin je dag met een heerlijk <br /> ontspannende verzorgingsroutine en <br /> verwen je huid met jouw favoriete producten.</p>
+                            <button>SHOP NU</button>
+                        </div>
+                        {this.state.arr.map((e) => {
+                            return <div className="sp-card" key={e}>
+                                <div className="img-cardsp">
+                                    <img src={`./assets/product${e}.webp`} width="100%" height="100%" />
+                                </div>
+                                <div className="content-cardsp">
+                                    <h1 className="product-title">Easter Egg Giftset</h1>
+                                    <p className="product-price">€8,50</p>
+                                </div>
+                            </div>
+                        })}
+                    </div>
+                </div>
+                <div className="card-sec-rit">
+                    <div className="inner-wrapper">
+                        <div className="all-cards-rit">
+                            <div className="card-rit" data-aos="fade-in">
+                                <div className="content-crit">
+                                    <span style={{ textTransform: "uppercase" }} className="heading-hss">EXCLUSIEF VOOR MY RITUALS MEMBERS</span>
+                                    <h3>Ons cadeau voor jou</h3>
+                                    <p className="p-cr">Ontvang een Jing set, <br /> bij besteding vanaf €40.</p>
+                                    <button>SHOP BESTSELLERS</button>
+                                </div>
+                                <div className="img-crit">
+                                    <img src="./assets/third.jpg" width="100%" height="100%" />
+                                </div>
+                            </div>
+                            <div className="card-rit" data-aos="fade-in">
+                                <div className="content-crit">
+                                    <h3>The Ritual of Shakura</h3>
+                                    <p className="p-cr">Voor iedereen is de perfecte geur te vinden in deze uitgebreide collectie van rijke, frisse en bloemige geuren voor in huis.</p>
+                                    <button>SHOP NU</button>
+                                </div>
+                                <div className="img-crit">
+                                    <img src="./assets/fourth.jpg" width="100%" height="100%" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="card-sec-rit">
+                    <div className="inner-wrapper">
+                        <div className="all-cards-rit">
+                            <div className="card-rit" data-aos="fade-in">
+                                <div className="content-crit">
+                                    <span style={{ textTransform: "uppercase" }} className="heading-hss">EXCLUSIEF VOOR MY RITUALS MEMBERS</span>
+                                    <h3>Ons cadeau voor jou</h3>
+                                    <p className="p-cr">Ontvang een Jing set, <br /> bij besteding vanaf €40.</p>
+                                    <button>SHOP BESTSELLERS</button>
+                                </div>
+                                <div className="img-crit">
+                                    <img src="./assets/third.jpg" width="100%" height="100%" />
+                                </div>
+                            </div>
+                            <div className="card-rit" data-aos="fade-in">
+                                <div className="content-crit">
+                                    <h3>The Ritual of Shakura</h3>
+                                    <p className="p-cr">Voor iedereen is de perfecte geur te vinden in deze uitgebreide collectie van rijke, frisse en bloemige geuren voor in huis.</p>
+                                    <button>SHOP NU</button>
+                                </div>
+                                <div className="img-crit">
+                                    <img src="./assets/fourth.jpg" width="100%" height="100%" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="last-sec-home">
+                    <div className="inner-wrapper" >
+                        <div className="img-lah">
+                            <img src="./assets/last.jpg" />
+                        </div>
+                        <div className="content-lah">
+                            <h3>Ontdek My Rituals</h3>
+                            <p>Ontdek de wereld van My Rituals en laat je verrassen. Je welkomstcadeau staat al voor je klaar!</p>
+                            <button>WORD MEMBER</button>
+                            <span>
+                                <SlowMotionVideoIcon style={{ marginRight: 6, fontSize: 27 }} />Bekijk De Video
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Home
