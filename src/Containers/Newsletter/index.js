54576761942f1557dc34b4cdc46a32eb6d208bac
= <=>
2020-04-22 16:17:49 +0500

    Some changes

--- src/Containers/Newsletter/index.js
+++ src/Containers/Newsletter/index.js
@@ -49,7 +49,63 @@ export class Newsletter extends Component {
                             </div>
                         </div>
                         <div className="account-right-content">
-                            
+                            <div className="section-title">
+                                <h2>
+                                    <span>
+                                        <DraftsIcon style={{ position: "absolute", transform: "translateY(-50%)", top: "50%", left: 2 }} />
+                                    </span>
+                                    <span>RITUALS COMMUNICATION</span>
+                                </h2>
+                            </div>
+                            <div className="main-content-wrapper">
+                                <div className="newsletter">
+                                    <div className="newsletter-content">
+                                        <p>
+                                            Here you manage all communication that you receive from Rituals.
+                                            <br />
+                                            Rituals will use your personal data as described in our <a>Privacy Policy</a> . By clicking 'save', I agree to the My Rituals <a>Terms and Conditions</a> .
+                                            <br />
+                                            If you want to terminate your My Rituals account, please <a>contact us</a> .
+                                            <br />
+                                            <br />
+                                            My Rituals newsletter
+                                        </p>
+                                        <label className="signup-label">
+                                            <div className="newsletter-checkbox">
+                                                <div className="inputfield-row">
+                                                    <input type="checkbox" checked className="input-checkbox" id="addtoemaillist" />
+                                                    <label for="addtoemaillist"></label>
+                                                </div>
+                                            </div>
+                                            <span className="newsletter-label">Yes, I would like to receive emails from Rituals with updates on member promotions and new products.</span>
+                                        </label>
+                                        <p>
+                                            My Rituals program
+                                        </p>
+                                        <label className="signup-label">
+                                            <div className="newsletter-checkbox">
+                                                <div className="inputfield-row">
+                                                    <input type="checkbox" checked className="input-checkbox" id="addtoemaillist" />
+                                                    <label for="addtoemaillist"></label>
+                                                </div>
+                                            </div>
+                                            <span className="newsletter-label">Yes, I would like to receive emails about exclusive gifts, inspiring articles and invitations to special Member events.</span>
+                                        </label>
+                                    </div>
+                                    <div className="newsletter-row">
+                                        <div>
+                                            <button>SAVE</button>
+                                        </div>
+                                        <div className="linked-email-container">
+                                            <p>
+                                                linked to:
+                                                <br />
+                                                maurice@blaiz.nl
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
