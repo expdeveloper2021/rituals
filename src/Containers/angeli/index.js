dd7892dd12c71bb6471a65e878fe2253d4215cc3
= <=>
2020-05-16 12:26:41 +0500

    Last page completed

--- src/Containers/angeli/index.js
+++ src/Containers/angeli/index.js
@@ -78,6 +78,60 @@ export class Angeli extends Component {
                                 </div>
                             </div>
                         </div>
+                        <div className="second-content-tsa">
+                            <div className="first-content-sct">
+                                <div className="image-fcsct">
+                                    <img src="http://www.zonjee-juwelier.nl/wp-content/uploads/2019/06/4.jpg" />
+                                </div>
+                                <div className="content-fcsct">
+                                    <h3>MOST BEAUTIFUL MOMENT</h3>
+                                    <p>
+                                        <font>The moment of an engagement or marriage should be unforgettable, a new discovery of life and that is exactly what drives</font>
+                                        <br />
+                                        <font>Angeli Di Bosca. The collections fuse exclusivity and class with a contemporary design and various designs for every budget.</font>
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="second-content-sct">
+                                <div className="image-scsct">
+                                    <img src="http://www.zonjee-juwelier.nl/wp-content/uploads/2019/06/5.jpg" />
+                                </div>
+                                <div className="content-scsct">
+                                    <h3>QUALITY FROM ATELIER</h3>
+                                    <p>Each ring is manufactured in our own workshop with the utmost care for quality and each model is available in 14 or 18 carat yellow gold or white gold and with or without brilliant.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    <section className="fourth-sec-adi">
+                        <div className="content-fsad">
+                            <div className="ci-fsad">
+                                <h3 style={{ fontSize: 36 }}>
+                                    DIAMOND
+                                </h3>
+                                <p>
+                                    <font>
+                                        Angeli di Bosca's rings are created in the world center of diamonds, Antwerp.
+                                    </font>
+                                    <br />
+                                    <font>
+                                        Special about these wedding rings is that the rings are made of recycled gold. Old jewelery and
+                                    </font>
+                                    <br />
+                                    <font>
+                                        precious metals are converted into pure raw materials and alloys, from which the wedding rings
+                                    </font>
+                                    <br />
+                                    <font>
+                                        are made. In addition, the rings have a perfect finish and there is an extraordinary eye for detail.
+                                    </font>
+                                    <br />
+                                    <font>
+                                        Which is also immediately noticeable when you take a look at the rings of this Belgian wedding ring brand.
+                                    </font>
+                                </p>
+                            </div>
+                        </div>
                     </section>
                 </div>
             </div>
